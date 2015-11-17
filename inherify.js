938d57714ef49b71124f49b0aa9d100b16465ba0
mickael.drymon@gmail.com <mdrymon@users.noreply.github.com>
2015-11-17 14:40:28 +0100

    Some good performance practice

--- inherify.js
+++ inherify.js
@@ -3,35 +3,41 @@
 //
 //     Copyright (c) 2015 mdrymon.
 //     The MIT License (MIT)
-(function () {
-  Inherify = {
+(function (global) {
+  var _global = global;
+  var Inherify = {
     factory : function (options) {
       if (typeof options !== 'object') throw new Error('Options must be an object');
-      if (options.element && typeof options.element !== 'object') throw new Error('Options.element must be an object');
-      if (options.prototypes && typeof options.prototypes !== 'object') throw new Error('Options.prototypes must be an object');
-      if (options.construct && typeof options.construct !== 'string') throw new Error('Options.construct must be a string or null');
-      if (options.extend && typeof options.extend !== 'string') throw new Error('Options.construct must be a string or null');
 
-      options.construct = options.construct || 'Inherify_BClass';
-      window[options.construct] = function () {
-        if (options.element) {
-          (Object.prototype.toString.call(options.params) === '[object Array]') && (options.params = []);
-          for (var prop in options.element) (prop !== 'init') && (this[prop] = options.element[prop]);
-          (typeof options.element.init === 'function') && options.element.init.apply(this, options.params);
+      var prop;
+      var _construct = options.construct || 'Inherify_BClass';
+      var element = options.element;
+      var params = options.params;
+      var prototypes = options.prototypes;
+      var extend = _global[options.extend];
+
+      if (element && typeof element !== 'object') throw new Error('Options.element must be an object');
+      if (prototypes && typeof prototypes !== 'object') throw new Error('Options.prototypes must be an object');
+      if (_construct && typeof _construct !== 'string') throw new Error('Options.construct must be a string or null');
+      if (extend && typeof extend !== 'function') throw new Error('Options.construct must be a string or null');
+
+      var _constructor = _global[_construct] = function (element, params) {
+        if (element) {
+          (Object.prototype.toString.call(params) === '[object Array]') && (params = []);
+          for (prop in element) element.hasOwnProperty(prop) && (prop !== 'init') && (this[prop] = element[prop]);
+          (typeof element.init === 'function') && element.init.apply(this, params);
         }
       }
-      if (options.extend && (typeof window[options.extend] === 'function')) {
-        this.parents[options.extend] = window[options.extend].prototype;
-        window[options.construct].prototype = new window[options.extend]();
-      }
-      for (var proto in options.prototypes) window[options.construct].prototype[proto] = options.prototypes[proto];
-      window[options.construct].prototype.constructor = window[options.construct];
-      var instance = new window[options.construct]();
-      return instance;
+      extend && (_constructor.prototype = new extend(element, params));
+      for (prop in prototypes) 
+        (prop !== 'constructor') && prototypes.hasOwnProperty(prop) &&
+        (_constructor.prototype[prop] = prototypes[prop]);
+      _constructor.prototype.constructor = _constructor;
+      return new _constructor(element, params);
     },
-    parents : {},
     base : function (extend) {
-      return this.parents[extend];
+      return _global[extend].prototype;
     }
-  }
-})();
+  };
+  global.Inherify = Inherify;
+})(this);
